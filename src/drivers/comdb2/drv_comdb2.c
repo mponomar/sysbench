12f8aa3402abcea675a90bee5441f11fbe3b0b43
Michael Ponomarenko <mponomarenko@bloomberg.net>
2017-09-02 08:55:51 -0400

    Builds with comdb2 driver

--- src/drivers/comdb2/drv_comdb2.c
+++ src/drivers/comdb2/drv_comdb2.c
@@ -19,10 +19,50 @@
 #include "sb_options.h"
 #include "db_driver.h"
 
+#include <cdb2api.h>
+
+int comdb2_drv_init(void);
+
+
 static sb_arg_t comdb2_drv_args[] = {
     SB_OPT_END
 };
 
+static drv_caps_t comdb2_drv_caps =
+{
+  .multi_rows_insert = 1,
+  .prepared_statements = 0,
+  .auto_increment = 0,
+  .needs_commit = 0,
+  .serial = 0,
+  .unsigned_int = 0
+};
+
+
+static int comdb2_drv_describe(drv_caps_t *caps)
+{
+  *caps = comdb2_drv_caps;
+
+  return 0;
+}
+
+int comdb2_drv_init(void) 
+{
+    return 0;
+}
+
+typedef struct
+{
+    cdb2_hndl_tp *db;
+} db_comdb2_conn_t;
+
+static int comdb2_drv_connect(db_comdb2_conn_t *db_comdb2_con) 
+{
+    int rc;
+    rc = cdb2_open(&db_comdb2_con->db, "mikedb", "local", 0);
+    if (rc)
+        return 0;
+}
 
 static db_driver_t comdb2_driver =
 {
@@ -30,10 +70,10 @@ static db_driver_t comdb2_driver =
   .lname = "Comdb2 driver",
   .args = comdb2_drv_args,
   .ops = {
-    .init = NULL,
+    .init = comdb2_drv_init,
     .thread_init = NULL,
-    .describe = NULL,
-    .connect = NULL,
+    .describe = comdb2_drv_describe,
+    .connect = comdb2_drv_connect,
     .disconnect = NULL,
     .reconnect = NULL,
     .prepare = NULL,
