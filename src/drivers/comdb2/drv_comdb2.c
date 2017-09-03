ccfd6909174423d1dee1e0ad890c3cd2d8af0ce4
Michael Ponomarenko <mponomarenko@bloomberg.net>
2017-09-03 12:14:57 -0400

    Start bound parameters.

--- src/drivers/comdb2/drv_comdb2.c
+++ src/drivers/comdb2/drv_comdb2.c
@@ -23,6 +23,8 @@
 
 #define DEBUG(format, ...) do { if (db_globals.debug) log_text(LOG_DEBUG, format, __VA_ARGS__); } while (0)
 
+static db_error_t comdb2_drv_query(struct db_conn *sb_conn, const char *query, size_t len, struct db_result *rs);
+
 static sb_arg_t comdb2_drv_args[] = {
     SB_OPT_END
 };
@@ -99,12 +101,96 @@ static int comdb2_drv_disconnect(struct db_conn *sb_conn)
 static int comdb2_drv_prepare(struct db_stmt *stmt, const char *query, size_t len)
 {
   DEBUG("%s(stmt=%p, query=%.*s, len=%d)", __func__, stmt, (int) len, query, (int) len);
+  stmt->query = strdup(query);
   return 0;
 }
+
+#define DO_NUMERIC_PARAM(var, cdb2type, field) \
+              values[i].type = cdb2type;                              \
+              if (params[i].is_null != NULL && *params[i].is_null)  { \
+                  values[i].addr= NULL;                               \
+                  values[i].len = 0;                                  \
+              }                                                       \
+              else {                                                  \
+                  memcpy(&var, params[i].buffer, sizeof(var));        \
+                  values[i].value.field = var;                        \
+                  values[i].addr = &values[i].value.field;            \
+                  values[i].len = sizeof(var);                        \
+              }
 
 /* bind params for prepared statements */
 static int comdb2_drv_bind_param(struct db_stmt *stmt, db_bind_t *params, size_t len)
 {
+  struct comdb2_bound_values{
+      cdb2_coltype type;
+      int len;
+      union {
+          int64_t ival;
+          char *strval;
+          double rval;
+          // TODO: datetime/interval types
+      } value;
+      int isnull;
+      void *addr;
+  } values[len];
+
+  cdb2_hndl_tp *db = (cdb2_hndl_tp*) stmt->connection->ptr;
+  cdb2_clearbindings(db);
+  stmt->bound_param = params;
+  stmt->bound_param_len = len;
+
+  for (size_t i = 0; i < len; i++) {
+      int8_t tiny;
+      int16_t small;
+      int32_t reg;
+      int64_t big;
+      float  f;
+      double d;
+
+      switch (params[i].type) {
+          case DB_TYPE_TINYINT:
+              DO_NUMERIC_PARAM(tiny, CDB2_INTEGER, ival);
+              break;
+          case DB_TYPE_SMALLINT:
+              DO_NUMERIC_PARAM(small, CDB2_INTEGER, ival);
+              break;
+          case DB_TYPE_INT:
+              DO_NUMERIC_PARAM(reg, CDB2_INTEGER, ival);
+              break;
+          case DB_TYPE_BIGINT:
+              DO_NUMERIC_PARAM(big, CDB2_INTEGER,ival);
+              break;
+          case DB_TYPE_FLOAT:
+              DO_NUMERIC_PARAM(f, CDB2_REAL, rval);
+              break;
+          case DB_TYPE_DOUBLE:
+              DO_NUMERIC_PARAM(d, CDB2_REAL, rval);
+              break;
+
+          case DB_TYPE_CHAR:
+          case DB_TYPE_VARCHAR:
+              if (params[i].is_null != NULL && *params[i].is_null)
+                  values[i].addr = NULL;
+              else {
+                  values[i].value.strval = malloc(*params[i].data_len);
+                  memcpy(values[i].value.strval, params[i].buffer, *params[i].data_len);
+                  values[i].addr = values[i].value.strval;
+                  values[i].len = *params[i].data_len;
+              }
+              break;
+
+          default:
+              log_text(LOG_FATAL, "Unknown type\n");
+              return DB_ERROR_FATAL;
+      }
+
+      int rc = cdb2_bind_index(db, i+1, values[i].type, values[i].addr, values[i].len);
+      // printf("%d  type %d  addr %p len %d\n", (int) i, values[i].type, values[i].addr, values[i].len);
+      if (rc) {
+          log_text(LOG_FATAL, "bind arg %d rc %d %s\n", (int) i, rc, cdb2_errstr(db));
+          return DB_ERROR_FATAL;
+      }
+  }
   DEBUG("%s(stmt=%p, params=%p, le=%d)", __func__, stmt, params, (int) len);
   return 0;
 }
@@ -119,8 +205,15 @@ static int comdb2_drv_bind_result(struct db_stmt *stmt, db_bind_t *params, size_
 /* execute prepared statement */
 static db_error_t comdb2_drv_execute(struct db_stmt *stmt, struct db_result *rs)
 {
-  // DEBUG("%s(stmt=%p, rs=%p)", __func__, stmt, rs);
-  return 0;
+  // rebind
+  int bindrc = comdb2_drv_bind_param(stmt, stmt->bound_param, stmt->bound_param_len);
+  if (bindrc) {
+      log_text(LOG_FATAL, "can't rebind params\n");
+      return DB_ERROR_FATAL;
+  }
+  db_error_t rc = comdb2_drv_query(stmt->connection, stmt->query, strlen(stmt->query), rs);
+  DEBUG("%s(stmt=%p, rs=%p) rc %d", __func__, stmt, rs, rc);
+  return rc;
 }
 
 /* fetch row for prepared statement */
@@ -153,7 +246,7 @@ static db_error_t comdb2_drv_query(struct db_conn *sb_conn, const char *query, s
       sb_conn->error = 0;
       sb_conn->sql_errno = rc;
       sb_conn->sql_errmsg = cdb2_errstr(db);
-      DEBUG("%s(sb_conn=%p, query=%.*s, len=%d, rs=%p) rc=%d err=%s", __func__, sb_conn, (int) len, query, (int) len, rs, rc, cdb2_errstr(db));
+      log_text(LOG_ALERT, "%s(sb_conn=%p, query=%.*s, len=%d, rs=%p) rc=%d err=%s", __func__, sb_conn, (int) len, query, (int) len, rs, rc, cdb2_errstr(db));
       if (rc == CDB2ERR_DUPLICATE)
           return 0;  // pretend this didn't happen
       else 
@@ -170,9 +263,10 @@ static int comdb2_drv_free_results(struct db_result *rs)
 }
 
 /* close prepared statement */
-static int comdb2_drv_close(struct db_stmt *sb_conn)
+static int comdb2_drv_close(struct db_stmt *stmt)
 {
-  DEBUG("%s(sb_conn=%p)", __func__, sb_conn);
+  DEBUG("%s(sb_conn=%p)", __func__, stmt);
+  stmt->bound_param = NULL;
   return 0;
 }
 
